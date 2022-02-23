d1a00cef052dde644e1e73390b2677ee1b410fe4
kamileecher2 <91373907+kamileecher2@users.noreply.github.com>
2022-02-23 17:55:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ SUDO_CHATS_ID = [-1001785091746]
 
 
 DRIVE_NAME = [
-    "MULTI",  # folder 1 name
-    "Cartoon",  # folder 2 name
+    "Multi",  # folder 1 name
+    "Kurslab",  # folder 2 name
     "Course",  # folder 3 name
     "Movies",  # ....
     "Series",  # ......
@@ -14,7 +14,7 @@ DRIVE_NAME = [
 
 DRIVE_ID = [
     "1UWUaUcy4U_7J1ZxY6WcI3j9mbb9EHfv0",  # folder 1 id
-    "12wNJTjNnR-CNBOTnLHqe-1vqFvCRLecn",  # folder 2 id
+    "1w2Bk_YusnY3Koka1HhI2imtHd8G0rm-8",  # folder 2 id
     "11nZcObsJJHojHYg43dBS0_eVvJrSD7Nf",  # and so onn... folder n id
     "10_hTMK8HE8k144wOTth_3x1hC2kZL-LR",
     "1-oTctBpyFcydDNiptLL09Enwte0dClCq",
@@ -23,7 +23,7 @@ DRIVE_ID = [
 
 INDEX_URL = [
     "https://kamileecher.ml/3:/TG",  # folder 1 index link
-    "https://dl.null.tech/0:/Cartoon",  # folder 2 index link
+    "https://kurslab.ml/1:/",  # folder 2 index link
     "https://dl.null.tech/0:/Course",  # and soo on folder n link
     "https://dl.null.tech/0:/MOVIES",
     "https://dl.null.tech/0:/Series",
